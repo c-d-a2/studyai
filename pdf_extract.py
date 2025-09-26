c780e7a139a4700ca119e4584663f030f240cb39
c-d-a2 <carlos.d.acostaaa@gmail.com>
2025-09-26 15:31:50 -0500

    removed comments and unnecessary libs

--- pdf_extract.py
+++ pdf_extract.py
@@ -1,5 +1,3 @@
-from pypdf import PdfReader
-
 import fitz
 
 def topic_parser(infile):
@@ -47,22 +45,10 @@ def extract_text(sections):
 
 
 
-def chunkenize(text):
-    chunker = RecursiveChunker()
-    chunks = chunker("Process Memory")
-
-    for chunk in chunks:
-        print(f"Chunk: {chunk}")
-        print(f"Tokens: {chunk.token_count}")
-
 
 toc2 = topic_parser("OperatingSystems.pdf")
 toc  = topic_parser("CCNP.pdf")         
 print(print_toc(toc))
 print(print_toc(toc2))
-#  Use topic and page marker on top of page
-#  Delimeter would be a summary
-#  using table of contents to guide 
-#  offset  
-#  how are the link formatted so that the pdf client can jump to the correct page? We can use this 
+
 
